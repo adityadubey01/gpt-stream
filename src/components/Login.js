fc43f10ba921ffe36b695ee1105fd159660e4f01
adityadubey01 <adityadubey412@gmail.com>
2023-11-25 18:38:49 +0530

    IntroducingConstant.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,6 +9,7 @@ import {
 import { auth } from "../utils/firebase";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { Background_Img, USER_AVATAR } from "../utils/constant";
 
 const Login = () => {
   const [isSignInForm, setIsSignForm] = useState(true);
@@ -36,7 +37,7 @@ const Login = () => {
           const user = userCredential.user;
           updateProfile(user, {
             displayName: name.current.value,
-            photoURL: "https://picsum.photos/200/300",
+            photoURL: USER_AVATAR,
           })
             .then(() => {
               const { uid, email, displayName, photoURL } = auth.currentUser;
@@ -82,10 +83,7 @@ const Login = () => {
     <div>
       <Header />
       <div className="absolute">
-        <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-          alt="Background_Img"
-        />
+        <img src={Background_Img} alt="Background_Img" />
       </div>
       <form
         onSubmit={(e) => {
