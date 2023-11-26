c314ae72f281f203b7567593bd5340e01b98f9ee
adityadubey01 <adityadubey412@gmail.com>
2023-11-26 11:22:45 +0530

    MovieStoreRedux

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,5 @@
+const TMDB_Authorization = process.env.REACT_APP_TMDB_TOKEN;
+
 export const LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
@@ -5,3 +7,11 @@ export const Background_Img =
   "https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
 export const USER_AVATAR = "https://picsum.photos/200/300";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: "Bearer " + TMDB_Authorization,
+  },
+};
