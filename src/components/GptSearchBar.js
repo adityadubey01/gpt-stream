5d52c070cb16032ce59433a02977abb74f6d08b1
adityadubey01 <adityadubey412@gmail.com>
2023-11-30 23:36:26 +0530

    GPTSearchFunctionality

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,18 +1,40 @@
 import { useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
+import { useRef } from "react";
+import openai from "../utils/openai";
 
 const GptSearchBar = () => {
   const langKey = useSelector((store) => store.config.lang);
+  const searchText = useRef(null);
+
+  const handleGptSearchClick = async () => {
+    const gptQuery =
+      "Act as a Movie Recommendation system and suggest some movies for the query: " +
+      searchText.current.value +
+      ". Only give me names of 5 Movies, comma separated like the example result given ahead. Example Results: Gadar, Shole, Don, Golmaal, Koi Mil Gaya";
+
+    const gptResults = await openai.chat.completions.create({
+      messages: [{ role: "user", content: gptQuery }],
+      model: "gpt-3.5-turbo",
+    });
+  };
 
   return (
     <div className="pt-[10%] flex justify-center">
-      <form className="w-1/2 bg-black grid grid-cols-12">
+      <form
+        className="w-1/2 bg-black grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
+          ref={searchText}
           type="text"
           className="p-4 m-4 col-span-9"
           placeholder={lang[langKey].gptSearchPlaceholder}
         />
-        <button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg">
+        <button
+          className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg"
+          onClick={handleGptSearchClick}
+        >
           {lang[langKey].search}
         </button>
       </form>
