44cd27213fcdab5d290445ab79eed439078a8ee8
adityadubey01 <adityadubey412@gmail.com>
2023-12-01 23:59:03 +0530

    GptSearchFunctionality

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,11 +1,26 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
 import { useRef } from "react";
 import openai from "../utils/openai";
+import { API_OPTIONS } from "../utils/constant";
+import { addGptMovieResult } from "../utils/gptSlice";
 
 const GptSearchBar = () => {
+  const dispatch = useDispatch();
   const langKey = useSelector((store) => store.config.lang);
   const searchText = useRef(null);
+
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        movie +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+
+    const json = await data.json();
+    return json.results;
+  };
 
   const handleGptSearchClick = async () => {
     const gptQuery =
@@ -13,12 +28,27 @@ const GptSearchBar = () => {
       searchText.current.value +
       ". Only give me names of 5 Movies, comma separated like the example result given ahead. Example Results: Gadar, Shole, Don, Golmaal, Koi Mil Gaya";
 
-    const gptResults = await openai.chat.completions.create({
-      messages: [{ role: "user", content: gptQuery }],
-      model: "gpt-3.5-turbo",
-    });
+    //Needs to be undone
+    // const gptResults = await openai.chat.completions.create({
+    //   messages: [{ role: "user", content: gptQuery }],
+    //   model: "gpt-3.5-turbo",
+    // });
 
-    const gptMovies = gptResults.choices?.[0]?.message?.content.split(",");
+    // const gptMovies = gptResults.choices?.[0]?.message?.content.split(",");
+    const gptMovies = [
+      "Andaz Apna Apna",
+      "Hera Pheri",
+      "Chupke Chupke",
+      "Jaane Bhi Do Yaaro",
+      "Padosan",
+    ];
+
+    const promiseArray = gptMovies.map((movie) => searchMovieTMDB(movie));
+
+    const tmdbResults = await Promise.all(promiseArray);
+    dispatch(
+      addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults })
+    );
   };
 
   return (
